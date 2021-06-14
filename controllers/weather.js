dde88e9ec644fae5d00275a403ad30958c3cbed9
Cuong Tran <cuong.tran@solteq.com>
2021-06-14 22:40:57 +0300

    change variable name from espoo to city, remove hardcode name

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,14 +1,12 @@
 const { fetchWeatherDataByCity } = require('../services/weather');
 
 
-// @des     Get espoo weather data
-// @router  GET /weather/espoo
+// @des     Get weather data
+// @router  GET /weather/:city
 // @access  public
-module.exports.getEspooWeather = async (req, res, next) =>
+module.exports.getCityWeather = async (req, res, next) =>
 {
-
     const city = req.params.city;
     const result = await fetchWeatherDataByCity(city);
-    console.log(result);
     res.render('weather/espoo', { title: 'Weather in Espoo', data: result });
 }
