18e395d22599ee78106763f3ec5f055a0df3d267
Cuong Tran <cuong.tran@solteq.com>
2021-06-17 19:58:41 +0300

    es 6 syntax, remove user router which doesn't need

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var weatherRouter = require('./routes/weather');
 
 var app = express();
@@ -22,17 +21,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/weather', weatherRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next)
+app.use((req, res, next) => 
 {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next)
+app.use((err, req, res, next) => 
 {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -44,7 +42,7 @@ app.use(function (err, req, res, next)
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function ()
+app.listen(port, () => 
 {
   console.log("server started successfullly already")
 })
--- controllers/weather.js
+++ controllers/weather.js
@@ -1,5 +1,15 @@
 const { fetchWeatherDataByCity } = require('../services/weather');
 
+// @desc     Get weather data
+// @router  GET /weather/index
+// @access  public
+module.exports.getWeatherIndex = async (req, res, next) =>
+{
+    const city = req.params.city;
+    const result = await fetchWeatherDataByCity(city);
+    res.render('weather/byCity', { title: `Weather in ${ city }`, data: result });
+}
+
 
 // @desc     Get weather data
 // @router  GET /weather/:city
