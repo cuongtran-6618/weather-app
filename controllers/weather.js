6976ac264cdd03b30acaf3c8ec51ee12d3cbd728
Cuong Tran <cuong.tran@solteq.com>
2021-06-28 12:06:36 +0300

    return json instead of rending the page

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,11 +1,19 @@
 const { fetchWeatherDataByCity } = require('../services/weather');
 
-// @desc     Get weather data
+// @desc     Get weather data for index page (duplicate with getCityWeather, should be refactor or remove)
 // @router  GET /weather/index
 // @access  public
 module.exports.getWeatherIndex = async (req, res, next) =>
 {
     const city = req.params.city;
+
+    if (!city || "" === city)
+    {
+        res.send({
+            "message": "error"
+        });
+    }
+
     const result = await fetchWeatherDataByCity(city);
     res.render('weather/byCity', { title: `Weather in ${ city }`, data: result });
 }
@@ -17,6 +25,14 @@ module.exports.getWeatherIndex = async (req, res, next) =>
 module.exports.getCityWeather = async (req, res, next) =>
 {
     const city = req.params.city;
+
+    if (!city || " " === city)
+    {
+        res.send({
+            "message": "error"
+        });
+    }
+
     const result = await fetchWeatherDataByCity(city);
-    res.render('weather/byCity', { title: `Weather in ${ city }`, data: result });
+    res.send(result);
 }
