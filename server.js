dae295ebbefaa05b4508b83e948ecb69c59dda76
Cuong Tran <cuong.tran@solteq.com>
2021-07-19 19:11:33 +0300

    rename file, relocate file

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const appBootstrap = require('./app');
-const boostrap = async () => {
-    console.log('boostrap');
-    const app = await appBootstrap();
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const connectDB = require('./boostrap/database');
 
-    const port = process.env.PORT || 3000;
+const indexRouter = require('./routes/index');
+const weatherRouter = require('./routes/weather');
+const errorHandler = require('./middleware/errorHandler');
 
-    app.listen(port, () => {
-        console.log('server started successfullly already');
+module.exports = async () => {
+    console.log('app called');
+    await connectDB();
+    const app = express();
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use('/', indexRouter);
+    app.use('/weather', weatherRouter);
+
+    // catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        next(createError(404));
     });
-};
 
-boostrap();
+    // error handler with custom error code in middleware
+    app.use(errorHandler);
+
+    return app;
+};
--- tests/e2e/weather.test.js
+++ tests/e2e/weather.test.js
@@ -1,11 +1,26 @@
 const request = require('supertest');
-const app = require('../app');
+const server = require('../../server');
+const mongoose = require('mongoose');
+
+beforeEach((done) => {
+    mongoose.connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => done()
+    );
+});
+
+afterEach((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+        mongoose.connection.close(() => done());
+    });
+});
 
 describe('GET /weather', () => {
     describe('Client visit espoo weather page', () => {
         // should reponse with 200
         test('should response with 200 status', async () => {
-            return request(app)
+            return request(server)
                 .get('/weather/espoo')
                 .expect(200)
                 .then((data) => {
