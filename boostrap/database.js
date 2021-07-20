06318ea943de967b44a00333260e1b338baa70a6
Cuong Tran <cuong.tran@solteq.com>
2021-07-20 15:43:24 +0300

    remove console command

--- boostrap/database.js
+++ boostrap/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        console.log('connect DB call');
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
